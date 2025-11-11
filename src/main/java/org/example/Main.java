384205978b2e66faf72a4a102d363d90a3e02d09
Aisuluu Omokoeva <aisuluuomokoeva@Mac.attlocal.net>
2025-11-10 19:32:52 -0600

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+       int number = 6;
+       for(int i = 0;i<number;i++){
+
+           System.out.println("Number is " + i);
+       }
 
     }
 }
