6bbb17d380c73eb5dcb5f596e15927063a6c2ad5
Aisuluu Omokoeva <aisuluuomokoeva@Mac.attlocal.net>
2025-11-11 18:23:57 -0600

    New class addad

---
+++ src/main/java/org/example/NewClass.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is new class");
+        System.out.println("This is new class");
+        System.out.println("This is new class");
+        System.out.println("This is new class");
+
+
+    }
+}
