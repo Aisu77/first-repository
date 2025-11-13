38d491f0ebef1d5b12ac2fd81fe9228419d6ae02
aisuluuomokoeva <aomokoeva@gmail.com>
2025-11-12 18:46:37 -0600

    student first commit

---
+++ src/main/java/org/example/StudentClass.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class StudentClass {
+
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+
+            System.out.println("Hello student");
+            System.out.println("Hello student");
+            System.out.println("Hello student");
+            System.out.println("Hello student");
+            System.out.println("Hello student");
+            System.out.println("Hello student");
+            System.out.println("Hello student");
+            System.out.println("Hello student");
+            System.out.println("Hello student");
+            System.out.println("Hello student");
+        }
+    }
+}
